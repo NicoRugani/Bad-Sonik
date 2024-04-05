1cdfa9abba79d242007fee5b956de29ffb0e1f24
NicoRugani <159579130+NicoRugani@users.noreply.github.com>
2024-04-05 13:12:49 -0500

    changed sum stuff :/

--- FirstGodot/PlayerMovement.cs
+++ FirstGodot/PlayerMovement.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class CharecterMovement : CharacterBody2D
+public partial class PlayerMovement : CharacterBody2D
 {
 	public const float Speed = 400.0f;
 	public const float JumpVelocity = -600.0f;
