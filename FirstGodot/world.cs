c5c9b608080a2ef6fd9fc62e29b9496a9a760537
NicoRugani <159579130+NicoRugani@users.noreply.github.com>
2024-04-07 15:25:07 -0500

    Random spawning of the ring scene

--- FirstGodot/world.cs
+++ FirstGodot/world.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class world : Node2D
 {
+	Random random = new Random();
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -15,12 +16,22 @@ public partial class world : Node2D
 	public override void _Process(double delta)
 	{
 	}
+
+	private float GetRandomXCoordinate(float minX, float maxX){
+
+		
+		return (float)random.NextDouble() * (maxX - minX) + minX;
+	}
 	
 	private void _on_spawn_timer_timeout()
 	{
+		
+		float randomX = GetRandomXCoordinate(0f, 1187f);
 		var scene = GD.Load<PackedScene>("res://ring.tscn");
-		var instance = scene.Instantiate();
+		var instance = (Node2D)scene.Instantiate();
+		
 		AddChild(instance);
+		instance.Position = new Vector2(randomX, 0);
 	}
 }
 
