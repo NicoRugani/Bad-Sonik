d1ec147e7619b1cbd82c5ec42f1a55785729c66a
NicoRugani <159579130+NicoRugani@users.noreply.github.com>
2024-04-08 13:12:55 -0500

    Stuff

--- FirstGodot/ring.cs
+++ FirstGodot/ring.cs
@@ -1,8 +1,10 @@
 using Godot;
 using System;
-
+using System.Diagnostics;
 public partial class ring : Node2D
 {
+	[Export]
+	RigidBody2D body {get;set;}
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,6 +13,12 @@ public partial class ring : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+
+	}
+	
+	private void _on_rigid_body_2d_body_entered(Node body)
+	{
 		
+		QueueFree();
 	}
 }
